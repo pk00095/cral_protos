ffff953af0a164b2382095a4d672f0bd3adad483
pk00095 <pk00095@gmail.com>
2020-08-19 01:58:23 +0530

    updates

--- demo.py
+++ demo.py
@@ -1,10 +1,46 @@
-from pipeline_pb2 import Status, ObjectDetectionPipeline
-from dataset_pb2 import dataset_meta, dataset_version_meta, tfrecord_meta #, ObjectDetectionDataFormat
+import pdb 
+from pipeline_pb2 import Status, Pipeline
 
+from dataset_pb2 import DatasetVersionMeta, TfrecordMeta, TaskType
+from dataset_pb2 import ObjectDetectionDataFormat
+from dataset_pb2 import DatasetMeta
+
+
+pipeline = Pipeline()
 
 #add data
+# dataset_meta = ObjectDetectionDatasetMeta()
+dataset_meta = DatasetMeta()
+
+dataset_meta.task_type = TaskType.OBJECT_DETECTION
+dataset_meta.object_detection = ObjectDetectionDataFormat.PASCALVOC
+dataset_meta.train_set_size = 1000
+dataset_meta.test_set_size = 500
+dataset_meta.num_classes = 5
+dataset_meta.version = 0
+
+pipeline.add_data = Status.RUNNING
+#assign dataset_meta
+pipeline.dataset_meta_info.CopyFrom(dataset_meta)
+
 #version data
 
+#lock_data
 
-if __name__ == '__main__':
-    parse()
+tfrecord_meta = TfrecordMeta()
+
+_NUM_SHARDS = 4
+tfrecord_meta.num_shards = _NUM_SHARDS
+
+for i in range(_NUM_SHARDS):
+    tfr_hash = tfrecord_meta.train_set.add()
+    tfr_hash.filename = f"{i}-of{_NUM_SHARDS}.tfrecord"
+    tfr_hash.hash = "xxxxxxxxxxxxxxxxxxx"
+
+#assign tfrecord_meta
+pipeline.tfrecord_meta_info.CopyFrom(tfrecord_meta)
+
+print(pipeline)
+
+# if __name__ == '__main__':
+#     parse()
